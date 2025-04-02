071287532463ab6f418ae3a45934681290c0a5de
Pratham2994 <prathampanchal02994@gmail.com>
2025-04-02 12:27:42 +0530

    Refactor getAllTeams controller to remove unnecessary comments and improve code clarity.

--- server/controller/getTeamsController.js
+++ server/controller/getTeamsController.js
@@ -4,18 +4,13 @@ const getAllTeams = async (req, res) => {
     try {
         const { _id } = req.body;
 
-        // Ensure _id is provided
         if (!_id) {
             return res.status(400).json({ error: 'User ID is required' });
         }
 
-        // Find teams where the user is a member
         const teams = await Team.find({ members: _id }).populate('members');
 
-        // Log the teams for debugging
-        console.log(teams);
 
-        // Return the teams in the response
         return res.status(200).json(teams);
     } catch (err) {
         console.error(err);
